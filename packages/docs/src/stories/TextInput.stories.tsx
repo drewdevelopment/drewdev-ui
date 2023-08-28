89ad40192620dfbffb5640a934fa69a7e76645fd
Andrew Gerez <andrewgerez18@gmail.com>
2023-08-27 23:13:37 -0300

    drewdev-ui: textarea

--- packages/docs/src/stories/TextInput.stories.tsx
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -8,7 +8,10 @@ export default {
   decorators: [
     (Story) => {
       return (
-        <Box as="label" css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}>
+        <Box 
+          as="label" 
+          css={{ display: 'flex', flexDirection: 'column', gap: '$2' }}
+        >
           <Text size="sm">Username</Text>
           {Story()}
         </Box>
