aad6fd6e673d070a433a1f3c4fb74fc84486d088
Andrew Gerez <andrewgerez18@gmail.com>
2023-08-28 23:21:31 -0300

    drewdev-ui: a11y

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -20,6 +20,7 @@ const config = {
     getAbsolutePath("@storybook/addon-onboarding"),
     getAbsolutePath("@storybook/addon-interactions"),
     getAbsolutePath("@storybook/addon-docs"),
+    getAbsolutePath("@storybook/addon-a11y"),
   ],
   framework: {
     name: getAbsolutePath("@storybook/react-vite"),
